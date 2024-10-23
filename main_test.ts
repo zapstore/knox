59c2d8625cb779c6c7ea47248fadd3c302b9a5d6
Alex Gleason <alex@alexgleason.me>
2024-10-22 21:26:12 -0500

    Add my fmt preferences

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,5 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals } from '@std/assert';
+import { add } from './main.ts';
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
